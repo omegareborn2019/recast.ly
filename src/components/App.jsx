a7494ada0cb651c52e537c195c64b07c287709dd
omega1984 <omegareborn2019@gmail.com>
2019-07-29 16:22:44 -0700

    did some work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import Search from './Search.js';
+import VideoPlayer from './VideoPlayer.js';
+import VideoList from './VideoList.js';
+
 var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <div><h5><em>Search</em><Search /></h5></div>
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em>VideoPlayer</em><VideoPlayer /></h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><h5><em>VideoList</em><VideoList /></h5></div>
       </div>
     </div>
   </div>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+import exampleVideoData from './data/exampleVideoData.js';
 
+// import VideoList from './components/VideoList.jsx';
+
+ReactDOM.render(<App />, document.getElementById("app"));
+console.log(exampleVideoData);
+// ReactDOM.render(<VideoList />, document.getElementsByClassName("col-md-5"));
